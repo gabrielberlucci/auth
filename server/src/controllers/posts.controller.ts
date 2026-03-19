01750a3502846c649ece94f49217b3348f9b425c
gabriel.berlucci <gabrielberlucci@proton.me>
2026-03-19 02:25:26 -0300

    feat: rename routes

---
+++ server/src/controllers/posts.controller.ts
@@ -0,0 +1,28 @@
+import type { NextFunction, Request, Response } from 'express';
+import { prisma } from '../../lib/prisma';
+
+export const getUserPosts = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const userId = (req as any).user.id;
+
+    const posts = await prisma?.post.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+
+    res.status(200).send({
+      message: 'User posts',
+      posts: posts,
+    });
+  } catch (error) {
+    res.status(500).send({
+      errorMessage: 'An error as occurred',
+      error: error,
+    });
+  }
+};
