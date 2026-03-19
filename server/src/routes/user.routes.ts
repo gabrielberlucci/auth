3efbd48824a1d02a94fad19277a44e3fb95ac7af
gabriel.berlucci <gabrielberlucci@proton.me>
2026-03-19 02:13:19 -0300

    feat: remove posts user from user routes

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { createUser, loginUser } from '@/controllers/user.controller';
 import { Router } from 'express';
 
 export const userRouter: Router = Router();
+export const postRouter: Router = Router();
 
 userRouter.post('/api/user', createUser);
 userRouter.post('/api/user/login', loginUser);
