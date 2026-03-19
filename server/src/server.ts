189fd69290247ba9d148966f2c6e951fac73a87f
gabriel.berlucci <gabrielberlucci@proton.me>
2026-03-19 02:24:38 -0300

    feat: remove other routes, and use routes index

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import { userRouter } from './routes/user.routes';
+import { routes } from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(userRouter);
+app.use(routes);
 
 const port = process.env.PORT || 3000;
 
